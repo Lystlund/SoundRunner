4ad99685f0882e08c9042bfdadd465d2c93a745e
Lystlund <raslystlund@gmail.com>
2014-03-28 14:13:48 +0100

    Unity Project Added

---
+++ SoundRunner/OSCtesting/Assets/OSC/soundstuff.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class soundstuff : MonoBehaviour {
+
+	private Dictionary<string, ServerLog> servers;
+	private float sendVar;
+	public GameObject enemy;
+
+	public float c = 34.0f;
+	//λ2 = λ (1 + v/c)	
+	public float v = 10.0f;	
+	public float λ = 440.0f;	
+	public float λ2 = 440.0f;	
+
+	// Use this for initialization
+	void Start () {
+
+		OSCHandler.Instance.Init(); //init OSC
+		OSCHandler.Instance.SendMessageToClient ("pdThing", "/127.0.0.1", sendVar);
+		//OSCHandler.Instance.SendMessage ("hey");
+
+		servers = new Dictionary<string, ServerLog>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//OSCHandler.Instance.UpdateLogs();
+		//servers = OSCHandler.Instance.Servers;
+
+ 
+		//foreach (Object respawn in respawns) 
+			//{ Instantiate(respawnPrefab, respawn.transform.position, respawn.transform.rotation) as GameObject;	 }
+
+
+		enemy = GameObject.FindGameObjectWithTag ("Enemy");	 
+		//Herot = GameObject.FindGameObjectsWithTag ("HeroTag");
+		float distance = Vector3.Distance(enemy.transform.position, transform.position);
+
+
+		if(distance>0){	 
+			λ2 = λ *((1 + v)/c);
+		}	 
+		if(distance<0){	 
+				λ2 = λ *((1 - v)/c);
+		}
+
+		sendVar = λ2;
+		Debug.Log (sendVar);
+
+		//testVar += Time.deltaTime*10;
+
+		OSCHandler.Instance.SendMessageToClient ("pdThing", "/127.0.0.1", sendVar);
+
+		// http://en.flossmanuals.net/pure-data/network-data/osc/ 
+	}
+}
---
+++ SoundRunner/OSCtesting/Assets/Scripts/Movement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	public float mSpeed = 0.3f;
+	public int ObsHit = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SphereCollider myCollider = transform.GetComponent<SphereCollider>();
+
+		//Forward speed	
+		transform.position += new Vector3(0,0,mSpeed);
+		
+		//Move left	
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			transform.position += new Vector3(-3.0f,0,0);
+		}
+		
+		//Move right
+		if(Input.GetKeyDown(KeyCode.D))
+		{
+			transform.position += new Vector3(3.0f,0,0);
+		}
+		
+		//jump	
+		if(Input.GetKeyDown(KeyCode.W) && transform.position.y < 1)
+		{
+			rigidbody.AddForce(new Vector3(0,300.0f,0));	
+		}
+		
+		//crouch	
+		if(Input.GetKey(KeyCode.S))
+		{
+			transform.localScale = new Vector3(1,0.5f,1); //as long as s is pressed scale to this size 
+			myCollider.radius = 0.25f;
+		}
+		else
+		{
+			transform.localScale = new Vector3(1, 1,1); //else be this size
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{	
+		if(other.gameObject.tag == "Enemy") //if you hit an enemy(obstacle) you loose one health
+		{
+			ObsHit = ObsHit + 1;
+			Debug.Log(ObsHit);
+		}
+	}
+}
