a3c52d2cc3b806cc755b2252d49ccbd07ff9a436
Lystlund <raslystlund@gmail.com>
2014-04-05 13:15:42 +0200

    Added Menu and start delay
    Made a new scene, which are being used as a start menu.
    For the game not to start to sudden a delay have been placed inside
    Movement script, so the controls first start after 1 second.

--- SoundRunner/OSCtesting/Assets/Scripts/Movement.cs
+++ SoundRunner/OSCtesting/Assets/Scripts/Movement.cs
@@ -4,46 +4,50 @@ using System.Collections;
 public class Movement : MonoBehaviour {
 	public float mSpeed = 0.3f;
 	public int ObsHit = 0;
+	bool justStarted = true;
+
+	IEnumerator WaitForStart() {
+		yield return new WaitForSeconds(1);
+		justStarted = false;
+	}
 
 	void Start () {
+
 	}
 
 	void Update () {
-		SphereCollider myCollider = transform.GetComponent<SphereCollider>();
+		if (justStarted == true) {
+			StartCoroutine (WaitForStart());
+		} 
+		else {
+			SphereCollider myCollider = transform.GetComponent<SphereCollider> ();
 
-		//Forward speed	
-		transform.position += new Vector3(0,0,mSpeed);
-		
-		//Move left	
-		if(Input.GetKeyDown(KeyCode.A) && transform.position.x>-2.8f)
-		{
-			transform.position += new Vector3(-3.0f,0,0);
-		}
-		
-		//Move right
-		if(Input.GetKeyDown(KeyCode.D) && transform.position.x<2.8f)
-		{
-			transform.position += new Vector3(3.0f,0,0);
-		}
-		
-		//jump	
-		if(Input.GetKeyDown(KeyCode.W) && transform.position.y < 0.5f)
-		{
-			rigidbody.AddForce(new Vector3(0,500.0f,0));	
-		}
-		
-		//crouch	
-		if(Input.GetKey(KeyCode.S))
-		{
-			transform.localScale = new Vector3(1,0.5f,1); //as long as s is pressed scale to this size 
-			myCollider.radius = 0.25f;
-		}
+			//Forward speed	
+			transform.position += new Vector3 (0, 0, mSpeed);
 
-		else
-		{
-			transform.localScale = new Vector3(1, 1,1); //else be this size
-		}	
+			//Move left	
+			if (Input.GetKeyDown (KeyCode.A) && transform.position.x > -2.8f) {
+					transform.position += new Vector3 (-3.0f, 0, 0);
+			}
 
+			//Move right
+			if (Input.GetKeyDown (KeyCode.D) && transform.position.x < 2.8f) {
+					transform.position += new Vector3 (3.0f, 0, 0);
+			}
+
+			//jump	
+			if (Input.GetKeyDown (KeyCode.W) && transform.position.y < 0.5f) {
+					rigidbody.AddForce (new Vector3 (0, 500.0f, 0));	
+			}
+
+			//crouch	
+			if (Input.GetKey (KeyCode.S)) {
+					transform.localScale = new Vector3 (1, 0.5f, 1); //as long as s is pressed scale to this size 
+					myCollider.radius = 0.25f;
+			} else {
+					transform.localScale = new Vector3 (1, 1, 1); //else be this size
+			}
+		}
 	}
 
 	void OnTriggerEnter(Collider other)
---
+++ SoundRunner/OSCtesting/Assets/Scripts/TextControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextControl : MonoBehaviour {
+
+	public bool isExitButton = false;
+
+	void OnMouseEnter(){
+		//Change color of text
+		renderer.material.color = Color.green;
+	}
+
+	void OnMouseExit(){
+		//Change color back to original color
+		renderer.material.color = Color.white;
+	}
+
+	void OnMouseUp(){
+		//Check if we are looking at Quit Button
+
+		if (isExitButton == true){
+			//quit the game
+			Application.Quit();
+		}
+
+		else{
+			//load level
+			Application.LoadLevel(1);
+		}
+	}
+}
