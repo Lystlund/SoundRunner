fd0429732523e7f456bef0c34a103cd2db827204
Lystlund <raslystlund@gmail.com>
2014-04-11 14:32:32 +0200

    Merge branch 'master' of https://github.com/Lystlund/SoundRunner
    Conflicts:
    	SoundRunner/OSCtesting/Assets/Scenes/MainGame.unity
    	SoundRunner/OSCtesting/Assets/Scenes/TutorialMultipleObjects.unity
    	SoundRunner/OSCtesting/OSCtesting.userprefs

--- SoundRunner/OSCtesting/Assets/Scripts/Movement.cs
+++ SoundRunner/OSCtesting/Assets/Scripts/Movement.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Text;
+using System.IO;
 
 public class Movement : MonoBehaviour {
 	public float mSpeed = 0.3f;
 	public int ObsHit = 0;
 	bool justStarted = true;
 	public int levelholder;
+	public long clockInit = 0;
 
 	IEnumerator WaitForStart() {
 		yield return new WaitForSeconds(1);
@@ -13,12 +16,38 @@ public class Movement : MonoBehaviour {
 	}
 
 	void Start () {
-
+		if (Application.loadedLevel == 1){
+			clockInit = System.DateTime.Now.Second;
+			StreamWriter sw1 = new StreamWriter("tilCSV.txt", true);
+			sw1.WriteLine("hit, time, zPos");
+			sw1.Close();
+		}
 	}
 
 	void Update () {
 		int level = Application.loadedLevel;
 		levelholder = level;
+
+
+		/*
+		if((Input.GetKeyDown(KeyCode.A)))	 {
+			StreamWriter sw1 = new StreamWriter("tilCSV.txt", true);
+			long rightNow = System.DateTime.Now.Second;
+			long diffRight = rightNow - clockInit;
+			string stringNow = diffRight.ToString();
+			sw1.WriteLine(stringNow + ",0");
+			sw1.Close();
+		}
+		if((Input.GetKeyDown(KeyCode.D)))	 {
+			StreamWriter sw1 = new StreamWriter("tilCSV.txt", true);
+			long rightNow = System.DateTime.Now.Second;
+			long diffRight = rightNow - clockInit;
+			string stringNow = diffRight.ToString();
+			sw1.WriteLine("0," + stringNow);
+			sw1.Close();
+		}
+		*/
+		
 		if (justStarted == true) {
 			StartCoroutine (WaitForStart());
 		} 
@@ -40,14 +69,18 @@ public class Movement : MonoBehaviour {
 
 			//jump	
 			if (Input.GetKeyDown (KeyCode.W) && transform.position.y < 0.8) {
-				rigidbody.AddForce(0, 8, 0, ForceMode.Impulse);
+				rigidbody.AddForce(0, 20, 0 , ForceMode.Impulse);
+			}
+			//if target gets to high, makes them fall down faster
+			if (transform.position.y > 2){
+				rigidbody.AddForce(0, -4, 0 , ForceMode.Impulse);
 			}
 
 			//crouch	
 			if (Input.GetKey (KeyCode.S)) {
 					transform.localScale = new Vector3 (1, 0.5f, 1); //as long as s is pressed scale to this size 
 					myCollider.radius = 0.25f;
-			} 
+			}
 			//normal size
 			else {
 					transform.localScale = new Vector3 (1, 1, 1); //else be this size
@@ -62,15 +95,25 @@ public class Movement : MonoBehaviour {
 		if (other.gameObject.tag == "LowOBS" || other.gameObject.tag == "MidOBS" || other.gameObject.tag == "HighOBS") { 
 			if (Application.loadedLevel == 1){
 			ObsHit = ObsHit + 1;
-			Debug.Log (ObsHit);
+			long rightNow = System.DateTime.Now.Second;
+			StreamWriter sw1 = new StreamWriter("tilCSV.txt", true);
+			string stringNow = System.DateTime.Now.Ticks.ToString();
+			sw1.WriteLine(ObsHit + ", " + (rightNow-clockInit) + ", " + transform.position.z);
+			sw1.Close();
 			}
+
 			else{
 				Application.LoadLevel(levelholder);
 			}
 		}
 
 		if (other.gameObject.tag == "Finish") {
-
+			if (Application.loadedLevel == 1){
+				ObsHit = ObsHit + 1;
+				StreamWriter sw1 = new StreamWriter("tilCSV.txt", true);
+				sw1.WriteLine("-------------------------- GAME ENDED --------------------------");
+				sw1.Close();
+			}
 			if (Application.loadedLevel == 3){
 				Application.LoadLevel(3);
 			}
